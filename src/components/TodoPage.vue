b0c6e3ff27ad662b9764a6499148e5cd2ff218e9
wlsgml961010@gmail.com <jhkim@intellicode.co.kr>
2021-02-07 17:04:07 +0900

    todolist - vue

---
+++ src/components/TodoPage.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="container">
+  <h2>Todo List</h2>
+  <div class="input-group" style="margin-bottom:10px;">
+    <input type="text" class="form-control" placeholder="Add to do" v-model="name" v-on:keyup.enter="createTodo(name)">
+    <span class="input-group-btn">
+      <button class="btn btn-default" type="button" @click="createTodo(name)">add</button>
+    </span>
+  </div>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="(todo, index) in todos">
+    {{todo.name}}
+      <div class="btn-group pull-right" style="font-size: 12px; line-height: 1;">
+        <button type="button" class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          더보기<span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href="#" @click="deleteTodo(index)">delete</a></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+<script>
+    export default {
+        name: 'TodoPage',
+        methods: {
+            deleteTodo(i) {
+                this.todos.splice(i,1);
+            },
+            createTodo(name) {
+                if(name != null) {
+                    this.todos.push({name:name});
+                    this.name = null
+                }
+            },
+            getTodos(){
+                this.$http.get('http://todos.jinny.xyz/api/todos')
+                .then((result)=>{
+                    vm.todos = result.data.data;
+                })
+            }
+            
+        },
+        data () {
+            return {
+               todos: []
+            };
+        },
+        mounted() {
+           this.getTodos();
+        }
+    }
+</script>
